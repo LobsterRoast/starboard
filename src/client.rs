53c25cf5bbfd77de0ff0fbc7fe7b2bbbd9fb30bd
lobsterroast <jekamminga@outlook.com>
2025-11-12 15:51:48 -0700

    refac: remove obsolete functions

--- src/client.rs
+++ src/client.rs
@@ -18,51 +18,6 @@ use chrono::{DateTime, Local, FixedOffset};
 use crate::util::*;
 use crate::debug;
 
-fn get_controller_message(controller: &GameController) -> String {
-    let name = controller.name();
-    let mapping = controller.mapping();
-    return format!("Controller found:\nName: {}\nMapping: {}\n", name, mapping);
-}
-
-fn get_contoller(controller_subsystem: GameControllerSubsystem) -> Result<GameController, &'static str> {
-    let joystick_count = controller_subsystem.num_joysticks().expect("Unable to count controllers.\n");
-    if joystick_count < 1 {
-        return Err("No controllers found to connect to.");
-    }
-
-    for i in 0..joystick_count {
-        if controller_subsystem.is_game_controller(i) {
-            return Ok(match controller_subsystem.open(i) {
-                Ok(v) => {
-                    debug!("{}", get_controller_message(&v));
-                    v
-                },
-                Err(e) => panic!("{}", e)
-            })
-        }
-    }
-    
-    Err("No valid controllers found to connect to.")
-}
-
-// uinput treats the Steam Deck as an analog input. SDL treats it as a binary one.
-// This function takes 2 binary inputs and outputs a signed integer to represent them.
-fn get_dpad_value(a: bool, b: bool) -> i32 {
-    if a & b {
-        return 0;  // both are activated
-    }
-
-    if !(a | b) {
-        return 0;  // neither are activated
-    }
-
-    if a {
-        return 1;  // a is activated but not b
-    }
-
-    return -1;  // b is activated but not a
-}
-
 fn get_formatted_time() -> String {
     let dt: DateTime<Local> = Local::now();
     format!("{}", dt.format("%Y,%m,%d,%H,%M,%S,%3f,%z"))
@@ -123,27 +78,14 @@ pub async fn client(framerate: Arc<u64>, ip: Arc<String>, port: Arc<u16>) {
     socket.connect(address).await.expect("Could not connect to the local network.\n");
     
     let sdl_context = sdl2::init().expect("Unable to initialize SDL.\n");
-
-    sdl2::hint::set("SDL_HINT_NO_SIGNAL_HANDLERS", "1");
-    
-    let controller_subsystem = sdl_context.game_controller().expect("Unable to enable SDL Game Controller Subsystem.\n");
-    sdl_context.joystick()
-        .expect("Could not get joystick subsystem.\n")
-        .set_event_state(true);
     
     let mut sdl_event_pump = sdl_context.event_pump().expect("Unable to generate event pump.");
-
-    let controller = match get_contoller(controller_subsystem) {
-        Ok(v) => v,
-        Err(e) => panic!("{}", e)
-    };
 
     let mut bitmask: u16 = 0;
     let mut axis_values: [i32; 8] = [0; 8];
     let key_associations: &HashMap<Button, u16> = get_key_associations();
     
     loop {
-
         sdl_event_pump.pump_events();
         for event in sdl_event_pump.poll_iter() {
             match event {
