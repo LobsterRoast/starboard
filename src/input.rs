622b934af0b99d3d2be10c8dc64921f5ab43f01d
lobsterroast <jekamminga@outlook.com>
2026-05-16 22:10:21 -0600

    feat: impl delta() for StarboardButtonStates

--- src/input.rs
+++ src/input.rs
@@ -1,3 +1,13 @@
 struct StarboardButtonStates {
+    // There are 25 different buttons in SDL3, requiring at least a u32 to cover them all.
     raw: u32,
+}
+
+impl StarboardButtonStates {
+    pub fn delta(&self, other: &StarboardButtonStates) -> u32 {
+        // delta() is symmetric.
+        // That is, A.delta(&B) == B.delta(&A).
+
+        self.raw ^ other.raw
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod input;
+mod server;
+
 fn main() {
     println!("Hello, world!");
 }
