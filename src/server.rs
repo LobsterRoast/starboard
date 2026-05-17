c439ffd07a97efdd8a3abba30e769684cb0337d5
lobsterroast <jekamminga@outlook.com>
2026-05-17 14:59:02 -0600

    feat: add properties to StarboardServer

--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,8 @@
-pub struct StarboardServer {}
+pub struct StarboardServer {
+    // The server is what will receive input packets from the controller and simulate a virtual
+    // joystick on another PC
+    address: String,     // i.e. {ip}:{port}
+    enabled_inputs: u32, // Bitmask representing all the enabled buttons on the server
+    enabled_axes: u32,   // Bitmask representing all the enabled axes on the
+                         // server
+}
