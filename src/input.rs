6a65b1d1933add082e8a6690d37b17977ab373c3
lobsterroast <jekamminga@outlook.com>
2026-05-19 22:08:35 -0600

    test: define InputTests module

--- src/input.rs
+++ src/input.rs
@@ -81,3 +81,13 @@ pub enum StarboardInput {
     Axis { id: u32, value: i32 },
     Button { id: u32, value: bool },
 }
+
+#[cfg(test)]
+mod InputTests {
+    use crate::input::{StarboardAxisStates, StarboardButtonStates};
+
+    const TEST_BUTTON_STATES: StarboardButtonStates = StarboardButtonStates { raw: 13 }; // 0b1101
+    const TEST_AXIS_STATES: StarboardAxisStates = StarboardAxisStates {
+        axes: [0, 145, 223, 1125, 102, 255],
+    };
+}
