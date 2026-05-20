43dce467bc54ff8224163a02f830ba0b753686f6
lobsterroast <jekamminga@outlook.com>
2026-05-20 16:34:32 -0600

    feat: impl gen_description() for VirtualJoystick

--- src/virtual_joystick.rs
+++ src/virtual_joystick.rs
@@ -1,7 +1,11 @@
+use anyhow::Result;
 use sdl3::{
     JoystickSubsystem, Sdl,
-    joystick::{Joystick, VirtualJoystickConnection},
+    gamepad::{Axis, Button},
+    joystick::{Joystick, VirtualJoystickConnection, VirtualJoystickDescription},
 };
+
+use crate::virtual_joystick;
 
 // Wrapper for virtual joystick interactions
 pub struct VirtualJoystick {
@@ -9,4 +13,19 @@ pub struct VirtualJoystick {
     joystick_subsystem: JoystickSubsystem,
     virtual_joystick_connection: VirtualJoystickConnection,
     virtual_joystick: Joystick,
+}
+
+impl VirtualJoystick {
+    // Generates the description that will be used to build a virtual joystick
+    fn gen_description<B, A>(&self, buttons: B, axes: A) -> VirtualJoystickDescription
+    where
+        B: IntoIterator<Item = Button>,
+        A: IntoIterator<Item = Axis>,
+    {
+        let desc = VirtualJoystickDescription::new()
+            .name("Starboard Virtual Gamepad")
+            .with_buttons(buttons)
+            .with_axes(axes);
+        desc
+    }
 }
