afc5782adf570a59e6004f1c94e3bc72a444c6f6
lobsterroast <jekamminga@outlook.com>
2026-05-20 14:26:38 -0600

    feat: add static `BINCODE_CONFIG` variable

--- src/datagram.rs
+++ src/datagram.rs
@@ -1,3 +1,7 @@
+use bincode::config::Configuration;
+
+static BINCODE_CONFIG: Configuration = bincode::config::standard();
+
 // Return a formatted address (i.e. 255.255.255.255:8080) or a specified default
 pub fn format_addr(ip: [u8; 4], port: u16) -> String {
     format!("{}.{}.{}.{}:{}", ip[0], ip[1], ip[2], ip[3], port)
