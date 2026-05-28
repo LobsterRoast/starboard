958edc8b327a0316ddf27da0e0dc5beb97eac916
lobsterroast <jekamminga@outlook.com>
2026-05-28 16:00:14 -0600

    test: add test_bitmask_iteration()

--- src/test/bitmask_test.rs
+++ src/test/bitmask_test.rs
@@ -7,3 +7,20 @@ fn test_bitmask_read_write() {
     bitmask.write_bit(0, true);
     assert!(bitmask.read_bit(0));
 }
+
+#[test]
+fn test_bitmask_iteration() {
+    let mut bitmask = Bitmask::new(5);
+    bitmask.write_bit(0, true);
+    bitmask.write_bit(3, true);
+
+    let mut i = 0;
+    for bit in bitmask {
+        if i == 0 || i == 3 {
+            assert!(bit);
+        } else {
+            assert!(!bit);
+        }
+        i += 1;
+    }
+}
