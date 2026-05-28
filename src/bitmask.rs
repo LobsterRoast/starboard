3c83c1a43aade007f983a869b14c784c00ae12da
lobsterroast <jekamminga@outlook.com>
2026-05-27 22:27:38 -0600

    feat: add Bitmask enum

---
+++ src/bitmask.rs
@@ -0,0 +1,6 @@
+pub enum Bitmask {
+    EIGHT { raw: u8 },
+    SIXTEEN { raw: u16 },
+    THIRTYTWO { raw: u32 },
+    SIXTYFOUR { raw: u64 },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod bitmask;
 mod datagram;
 mod input;
 mod server;
