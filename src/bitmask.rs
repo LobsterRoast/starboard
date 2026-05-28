e07694c09698b66c49ed7445aea9853f44c85b09
lobsterroast <jekamminga@outlook.com>
2026-05-28 12:46:37 -0600

    feat: impl new() for Bitmask

--- src/bitmask.rs
+++ src/bitmask.rs
@@ -1,3 +1,5 @@
+use core::usize;
+
 enum BitmaskRaw {
     EIGHT { raw: u8 },
     SIXTEEN { raw: u16 },
@@ -10,4 +12,21 @@ enum BitmaskRaw {
 // interface for using a bitmask
 pub struct Bitmask {
     raw: BitmaskRaw,
+    size: usize,
+}
+
+impl Bitmask {
+    pub fn new(size: usize) -> Self {
+        let mut raw = BitmaskRaw::EIGHT { raw: 0 };
+        if size > 64 {
+            raw = BitmaskRaw::ONETWENTYEIGHT { raw: 0 };
+        } else if size > 32 {
+            raw = BitmaskRaw::SIXTYFOUR { raw: 0 };
+        } else if size > 16 {
+            raw = BitmaskRaw::THIRTYTWO { raw: 0 };
+        } else if size > 8 {
+            raw = BitmaskRaw::SIXTEEN { raw: 0 };
+        }
+        Self { raw, size }
+    }
 }
