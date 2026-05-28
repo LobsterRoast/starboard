f50f5e3d57cda9f825c74d70fcdc196d258bc8b5
lobsterroast <jekamminga@outlook.com>
2026-05-28 12:37:14 -0600

    refac: turn `pub enum Bitmask` into `enum BitmaskRaw`

--- src/bitmask.rs
+++ src/bitmask.rs
@@ -1,6 +1,7 @@
-pub enum Bitmask {
+enum BitmaskRaw {
     EIGHT { raw: u8 },
     SIXTEEN { raw: u16 },
     THIRTYTWO { raw: u32 },
     SIXTYFOUR { raw: u64 },
+    ONETWENTYEIGHT { raw: u128 },
 }
