84eb8facd1277801d33d69dca5bb774ed2122333
Joel Kamminga <jekamminga@outlook.com>
2026-06-03 23:04:03 -0600

    feat: create function to broadcast client presence

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,23 @@
+use anyhow::Result;
+use tokio::net::UdpSocket;
+use tokio::time::{Duration, sleep};
+
 pub struct StarboardClient {
     id: u64,
     port: u16,
+}
+
+impl StarboardClient {
+    // Broadcast a client's presence to server's on the local network
+    async fn broadcast_id(&self) -> Result<()> {
+        let addr = format!("0.0.0.0:{}", self.port);
+        let mut socket = UdpSocket::bind(addr).await?;
+        socket.set_broadcast(true);
+        // TODO: Create a method to generate a packet to broadcast
+        let packet: [u8; 1] = [0; 1];
+        loop {
+            sleep(Duration::from_secs(15));
+            socket.send(&packet).await?;
+        }
+    }
 }
