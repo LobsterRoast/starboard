6cdfb20abdf45d5eb471b29f96d1123e28830f40
Joel Kamminga <jekamminga@outlook.com>
2026-06-17 00:11:08 -0600

    feat: add skeleton of `StarboardServerUI::launch_ui`

--- src/server_ui.rs
+++ src/server_ui.rs
@@ -3,3 +3,10 @@ use anyhow::Result;
 // The struct to manage the UI that opens when the server application is opened
 // The UI is created through `ratatui` and runs in a terminal
 pub struct StarboardServerUI {}
+
+impl StarboardServerUI {
+    pub fn launch_ui(&self) -> Result<()> {
+        ratatui::run(|mut terminal| {});
+        Ok(())
+    }
+}
