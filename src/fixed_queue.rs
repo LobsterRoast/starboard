f2a5f4b55a6af33487a71c795091b7b4f33da1f0
Joel Kamminga <jekamminga@outlook.com>
2026-06-19 18:03:31 -0600

    feat: impl `Index` and `IndexMut` for `FixedQueue<T>`

--- src/fixed_queue.rs
+++ src/fixed_queue.rs
@@ -1,4 +1,7 @@
-use std::collections::VecDeque;
+use std::{
+    collections::VecDeque,
+    ops::{Index, IndexMut},
+};
 
 // This is a queue with a maximum size, such that - if a push should cause it to exceed the maximum
 // size - an element will be popped from the front of the queue until the capacity is met. This is
@@ -23,5 +26,19 @@ impl<T> FixedQueue<T> {
         if self.inner.len() > self.capacity {
             self.inner.pop_front();
         }
+    }
+}
+
+impl<T> Index<usize> for FixedQueue<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.inner[index]
+    }
+}
+
+impl<T> IndexMut<usize> for FixedQueue<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.inner[index]
     }
 }
