86de9588a1e849e24bb309689fde6efbf917aa80
Joel Kamminga <jekamminga@outlook.com>
2026-06-19 18:20:10 -0600

    feat: derive `Debug` on `FixedQueue<T>`

--- src/fixed_queue.rs
+++ src/fixed_queue.rs
@@ -6,6 +6,7 @@ use std::{
 // This is a queue with a maximum size, such that - if a push should cause it to exceed the maximum
 // size - an element will be popped from the front of the queue until the capacity is met. This is
 // used to keep track of things like latency data.
+#[derive(Debug)]
 pub struct FixedQueue<T> {
     inner: VecDeque<T>,
     capacity: usize,
