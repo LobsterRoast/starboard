2754e0b95841190cf2310c58b56e63f3ac750927
Joel Kamminga <jekamminga@outlook.com>
2026-07-08 13:08:27 -0600

    feat: implement a constructor for `DummySteamDeck`

--- src/dummy_steam_deck.rs
+++ src/dummy_steam_deck.rs
@@ -1,6 +1,23 @@
-use crate::evdev_sb::VirtualJoystick;
+use crate::bitmask::Bitmask;
+use crate::evdev_sb::{SUPPORTED_AXES, SUPPORTED_BUTTONS, VirtualJoystick, VirtualJoystickBuilder};
 
-struct DummySteamDeck {
+pub struct DummySteamDeck {
     inner: VirtualJoystick,
 }
 
+impl DummySteamDeck {
+    pub fn new() -> Self {
+        let buttons: Bitmask = SUPPORTED_BUTTONS.iter().map(|button| *button).collect();
+        let axes: Bitmask = SUPPORTED_AXES.iter().map(|axis| *axis).collect();
+        Self {
+            inner: VirtualJoystickBuilder::new()
+                .unwrap()
+                .enable_buttons_bitmask(buttons)
+                .unwrap()
+                .enable_axes_bitmask(axes)
+                .unwrap()
+                .build()
+                .unwrap(),
+        }
+    }
+}
