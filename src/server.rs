f1af06b2a0251837a78618fcdc5517fa653e46b9
Joel Kamminga <jekamminga@outlook.com>
2026-08-04 22:29:03 -0600

    fix: fix issue where UI shows ping to be higher than it actually is

--- src/server.rs
+++ src/server.rs
@@ -7,8 +7,12 @@ use tokio::net::UdpSocket;
 
 use evdev::{AbsoluteAxisCode, KeyCode};
 
-use tokio::sync::{Mutex, MutexGuard};
-use tokio::task::JoinSet;
+use tokio::{
+    select,
+    sync::{Mutex, MutexGuard},
+    task::JoinSet,
+    time::{Interval, interval},
+};
 use tokio_util::sync::CancellationToken;
 
 use chrono::{DateTime, Local};
@@ -283,14 +287,31 @@ async fn manage_controllers(
     let addr = format!("0.0.0.0:{}", port);
     let sock = UdpSocket::bind(addr).await?;
     let mut raw: [u8; 256] = [0; 256];
+    let mut interval = interval(Duration::from_secs(1));
     loop {
-        {
-            let mut detected_controllers = detected_controllers.lock().await;
-            manage_controller_timeouts(&mut detected_controllers);
-            detect_controllers(&mut detected_controllers, &sock, &mut raw).await?;
-        }
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        wait_for_controller_update(&detected_controllers, &sock, &mut interval, &mut raw).await?;
     }
+}
+
+// Wait for a tick or for a packet to be received, and then update controller diagnostics
+// accordingly
+async fn wait_for_controller_update(
+    detected_controllers: &Arc<Mutex<ControllerMap>>,
+    socket: &UdpSocket,
+    interval: &mut Interval,
+    raw: &mut [u8; 256],
+) -> Result<()> {
+    tokio::select! {
+        _ = interval.tick() => {}
+        _ = socket.recv(raw) => {
+            let packet: BroadcastPacket = deserialize(Vec::from(&mut *raw))?;
+            insert_controller(&mut detected_controllers.lock().await, packet);
+        }
+    }
+    let mut detected_controllers = detected_controllers.lock().await;
+    manage_controller_timeouts(&mut detected_controllers);
+    detect_controllers(&mut detected_controllers, &socket, raw).await?;
+    Ok(())
 }
 
 // For each controller, ensures that it is not timed out
@@ -317,7 +338,10 @@ async fn detect_controllers(
 }
 
 // Inserts a controller at key `id` if it does not already exist
-fn insert_controller(detected_controllers: &mut ControllerMap, packet: BroadcastPacket) {
+fn insert_controller(
+    detected_controllers: &mut MutexGuard<'_, ControllerMap>,
+    packet: BroadcastPacket,
+) {
     if !detected_controllers.contains_key(packet.id()) {
         let diagnostic: ControllerDiagnostic = packet.into();
         detected_controllers.insert(*diagnostic.id(), diagnostic);
